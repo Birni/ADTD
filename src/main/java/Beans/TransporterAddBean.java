5908a24d1a8add4c2988f01d1e58b9e71c013692
Birni <Matthias-Birnthaler@hotmail.de>
2019-12-27 12:34:24 +0100

    option to add transporter

---
+++ src/main/java/Beans/TransporterAddBean.java
@@ -0,0 +1,63 @@
+package Beans;
+
+import services.VO.ITransporter;
+import services.entity.Transporter;
+import services.service.VirtualTransporterManager;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="beanTransporterDetails")
+@SessionScoped
+public class TransporterDetailsBean{
+
+    public long ID;
+    public float payload;
+    public float battery;
+
+    private int lastindex;
+
+    public long getID() {
+        return ID;
+    }
+
+    public float getBattery() {
+        return battery;
+    }
+
+    public float getPayload() {
+        return payload;
+    }
+
+    public TransporterDetailsBean()
+    {
+        ITransporter transporter = VirtualTransporterManager.getInstance().GetTransporterFromTransporterList(0);
+        SetBeanAttributes(transporter);
+    }
+
+
+    public void NextAction() {
+
+        ITransporter transporter = VirtualTransporterManager.getInstance().GetTransporterFromTransporterList(lastindex);
+        SetBeanAttributes(transporter);
+
+    }
+
+    private void SetBeanAttributes(ITransporter transporter)
+    {
+        if(null != transporter)
+        {
+            ID = ((Transporter) transporter.object).getId();
+            payload = ((Transporter)transporter.object).getPayload();
+            battery = ((Transporter)transporter.object).getBattery();
+
+            lastindex = transporter.index;
+        }
+        else
+        {
+            // TODO: Handle no transporter at all
+        }
+
+    }
+
+}
